7a651bb150100588409e42fb309230c0655b5f44
hanhongbin <hanhb1994924@qq.com>
2017-08-25 20:25:25 +0800

    commit.08.25

--- routes/input2.js
+++ routes/input2.js
@@ -1,8 +1,42 @@
 var express = require('express');
 var router = express.Router();
-
-router.get('/', function(req, res, next) {
-    res.render('input2');
+var mysql = require('mysql');
+var config = require('../config');
+var pool = mysql.createPool(config.mysql);
+router.get('/', function(req, res) {
+    pool.getConnection(function (err,connection) {
+        var sql = 'SELECT * FROM apply order by aid DESC limit 1';
+        connection.query(sql, function (err, result) {
+            connection.release();
+            var string=JSON.stringify(result);
+            var input1=JSON.parse(string);
+            res.render('input2', {input1:input1});
+        });
+    })
+});
+router.post('/input2post',function (req,res) {
+    var did = req.body.did;
+    var aid = req.body.aid;
+    var dwh_date = req.body.dwh_date;
+    console.log(aid);
+    pool.getConnection(function (err,connection) {
+        var sql = 'insert into dangwei (id, aid, did, dwh_date) values (0, ?, ?, ?)';
+        connection.query(sql,[aid, did, dwh_date],function (err,result) {
+            if(err)
+            {result = {
+                code: 300,
+                msg: '党委会相关信息提交成功'};
+                console.log(err)
+            }
+            else
+            {result = {
+                code: 200,
+                msg: '党委会信息提交失败，请重新输入'}
+            }
+            res.json(result);
+        });
+        connection.release();
+    })
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,14 +3,17 @@ var router = express.Router();
 var mysql = require('mysql');
 var config = require('../config');
 var pool = mysql.createPool(config.mysql);
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     pool.getConnection(function (err,connection) {
         var sql1 = 'SELECT * FROM user';
         connection.query(sql1, function (err, result) {
             connection.release();
             var string=JSON.stringify(result);
-            var result=JSON.parse(string);
-            res.render('user', {persons:result});
+            var result1=JSON.parse(string);
+            res.render('user', {persons:result1});
+            console.log(result);
+            console.log(string);
+            console.log(result1);
         });
     })
 });
